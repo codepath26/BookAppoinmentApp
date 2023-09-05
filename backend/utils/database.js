9d4785ac70cbf0f9adb6d98bb0d207f155476cf1
PARTH THAKOR <129299328+codepath26@users.noreply.github.com>
2023-09-05 15:14:09 +0530

    Update database.js

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,3 +1,4 @@
+  require('dotenv').config();
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USER,process.env.DATABASE_PASS,{
   host : 'localhost',
